8681769716e0b9201112898e84c49ed88cd495ba
Luca <colazolucavalentino99@gmail.com>
2026-02-16 16:07:47 -0300

    cambios en archivos duplicados

--- components/index.ts
+++
@@ -1,6 +0,0 @@
-// Barrel exports para componentes reutilizables
-export { Navbar } from "./layout/Navbar";
-export { PosterCarousel } from "./carousel/PosterCarousel";
-export { TextField, PasswordField } from "./auth/AuthFields";
-export { SignupForm } from "./auth/SignupForm";
-export { ResetPasswordForm } from "./auth/ResetPasswordForm";
